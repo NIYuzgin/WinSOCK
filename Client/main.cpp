7466333783039b2aeb5c8348e6f1ba7c8d0910f6
Николай Немов <prost91@outlook.com>
2026-06-02 22:02:22 +0300

    FormatLastError completely DONE

--- Client/main.cpp
+++ Client/main.cpp
@@ -8,31 +8,16 @@
 #include<WinSock2.h>
 #include<WS2tcpip.h>
 #include<iphlpapi.h>
+#include<FormatLastError.h>
 
 using namespace std;
 
 #pragma comment(lib, "WS2_32.lib")
+#pragma comment(lib, "FormatLastError.lib")
 
 #define PORT "27015"
 #define BUFFER_LENGTH	1500
 
-LPSTR FormatLastError(DWORD dwError, CHAR szBuffer[])
-{
-	LPSTR lpBuffer = NULL;
-	FormatMessage
-	(
-		FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
-		NULL,
-		dwError,
-		MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
-		(LPSTR)&lpBuffer,
-		256,
-		NULL
-	);
-	sprintf(szBuffer, "Error %i: %s", dwError, lpBuffer);
-	LocalFree(lpBuffer);
-	return szBuffer;
-}
 
 void main()
 {
@@ -70,6 +55,7 @@ void main()
 	SOCKET connect_socket = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
 	if (connect_socket == INVALID_SOCKET)
 	{
+		cout << FormatLastError(WSAGetLastError(), szError) << endl;
 		cout << "Socket creation error: " << WSAGetLastError() << endl;
 		freeaddrinfo(result);
 		WSACleanup();
@@ -81,9 +67,7 @@ void main()
 	if (iResult == SOCKET_ERROR)
 	{
 		DWORD dwError = WSAGetLastError();
-		
 		//CHAR szBuffer[256] = {};
-		
 		cout << "Unable to connect to Server." << endl;
 		//cout << "Error " << dwError; // << ":\t" << lpBuffer << endl;
 		cout << FormatLastError(dwError, szError) << endl;
@@ -101,7 +85,8 @@ void main()
 	iResult = send(connect_socket, sendbuffer, strlen(sendbuffer), 0);
 	if (iResult == SOCKET_ERROR)
 		{
-			cout << "Send failed:\t" << WSAGetLastError() << endl;
+		cout << FormatLastError(WSAGetLastError(), szError) << endl;
+		cout << "Send failed:\t" << WSAGetLastError() << endl;
 			closesocket(connect_socket);
 			freeaddrinfo(result);
 			WSACleanup();
@@ -114,13 +99,14 @@ void main()
 		iResult = recv(connect_socket, recvbuffer, BUFFER_LENGTH, 0);
 		if (iResult > 0)cout << recvbuffer << "(" << iResult << " Bytes)" << endl;
 		else if (result == 0) cout << "Connection closed" << endl;
-		else cout << "Receive failed:\t" << WSAGetLastError() << endl;
+		else cout << FormatLastError(WSAGetLastError(), szError) << endl; //cout << "Receive failed:\t" << WSAGetLastError() << endl;
 			
 	} while (iResult > 0);
 		
 	iResult = shutdown(connect_socket, SD_BOTH);
 	if (iResult == SOCKET_ERROR)
 	{
+		cout << FormatLastError(WSAGetLastError(), szError) << endl;
 		cout << "Shutdown failed: " << WSAGetLastError() << endl;
 		closesocket(connect_socket);
 		freeaddrinfo(result);
