cbe66060c2df9edb4cb7216437ede2ffc96c9bd6
Николай Немов <prost91@outlook.com>
2026-06-03 17:04:32 +0300

    Russian OK

--- Client/main.cpp
+++ Client/main.cpp
@@ -80,10 +80,11 @@ void main()
 
 	//5) Отправка и получение данных:
 	CHAR sendbuffer[BUFFER_LENGTH] = "Hello Server";
-	CHAR recvbuffer[BUFFER_LENGTH] = {};
 
 	do
 	{
+		
+		CHAR recvbuffer[BUFFER_LENGTH] = {};
 		iResult = send(connect_socket, sendbuffer, strlen(sendbuffer), 0);
 		if (iResult == SOCKET_ERROR)
 		{
@@ -104,7 +105,10 @@ void main()
 			else cout << FormatLastError(WSAGetLastError(), szError) << endl; //cout << "Receive failed:\t" << WSAGetLastError() << endl;
 
 		//} while (iResult > 0);
-		cin.getline(sendbuffer, BUFFER_LENGTH);
+			ZeroMemory(sendbuffer, BUFFER_LENGTH);
+			SetConsoleCP(1251);
+			cin.getline(sendbuffer, BUFFER_LENGTH);
+			SetConsoleCP(866);
 	} while (strcmp(sendbuffer, "exit")!=0);
 		
 	iResult = shutdown(connect_socket, SD_BOTH);
